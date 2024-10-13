1e9f5e45d0c900bf10cbf3cd3162a0946849a4ed
Mitchell Johnson <mitchell.johnson@students.snow.edu>
2024-10-12 20:45:49 -0600

    Week 7 proj blazor unfinished

--- Logic/IRoad.cs
+++ Logic/IRoad.cs
@@ -1,8 +1,39 @@
-public interface IRoad
+namespace Logic
 {
-  public int RowCount { get; }
-  public int ColCount { get; }
-
-  void SetVehicle(int row, int column, IVehicle vehicle);
-  IVehicle? GetVehicle(int row, int column);
+    public interface IRoad
+    {
+        int RowCount { get; }
+        int ColCount { get; }
+        IVehicle GetVehicle(int row, int col);
+        void SetVehicle(int row, int column, IVehicle vehicle);
+    }
 }
+
+namespace Logic
+{
+    public class Road : IRoad
+    {
+        private IVehicle[] vehicles;
+
+        public int RowCount { get; }
+        public int ColCount { get; }
+
+        public Road(int rows, int columns)
+        {
+            RowCount = rows;
+            ColCount = columns;
+            vehicles = new IVehicle[rows, columns];
+        }
+
+        public IVehicle GetVehicle(int row, int col)
+        {
+            return vehicles[row, col];
+        }
+
+        public void SetVehicle(int row, int col, IVehicle vehicle)
+        {
+            vehicles[row, col] = vehicle;
+        }
+    }
+}
+
--- Logic/IVehicle.cs
+++ Logic/IVehicle.cs
@@ -1,4 +1,51 @@
-public interface IVehicle
+
+namespace Logic
 {
-  public string GetImagePath();
+    public interface IVehicle
+    {
+        string GetImagePath();
+    }
+}
+
+namespace Logic
+{
+    public class Car : IVehicle
+    {
+        public string GetImagePath()
+        {
+            return "car";
+        }
+    }
+}
+
+namespace Logic
+{
+    public class Bus : IVehicle
+    {
+        public string GetImagePath()
+        {
+            return "bus";
+        }
+    }
+}
+
+namespace Logic
+{
+    public class FireTruck : IVehicle
+    {
+        public string GetImagePath()
+        {
+            return "firetruck";
+        }
+    }
+}
+namespace Logic
+{
+    public class Taxi : IVehicle
+    {
+        public string GetImagePath()
+        {
+            return "taxi";
+        }
+    }
 }
